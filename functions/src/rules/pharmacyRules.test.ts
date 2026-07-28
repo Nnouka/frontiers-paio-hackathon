94efbddcbc6fd1516f6cec815e08eb60cdffe318
Nnouka Stephen <nnoukastephen@gmail.com>
2026-07-28 15:50:53 +0200

    Remove unused expect import from pharmacy rules test

--- functions/src/rules/pharmacyRules.test.ts
+++ functions/src/rules/pharmacyRules.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { describe, it, beforeAll, afterAll } from "vitest";
 import * as fs from "fs";
 import * as path from "path";
 import {
